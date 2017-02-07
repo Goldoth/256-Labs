b370221d9fe9ce9707ad8f62da32de4e7b30febb
Will Debus <wmdebus@syr.edu>
2017-02-07 15:46:23 -0500

    Everything up to lesson 3

--- content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
+++ content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
@@ -9,5 +9,14 @@ http://www.madlibs.com and http://www.madtakes.com
 Your story should take at least 5 inputs, and should include more than
 once sentence. 
 '''
-
+verb1 = input("Enter a verb ending in ing")
+adj1 = input("Enter a verb")
+verb2 = input("Enter a verb")
+adj2 = input("Enter an adjective ending in ly")
+adj3 = input("Enter an adjective ending in ly")
+verb3 = input("Enter a verb ending in ing")
+print("There once was a person",verb1, "in a pointless class. He was tired of the", adj1, "GA's never paying attention to him.")
+print("All he wanted them to do was to", verb2, "his", adj2, "homework so he could leave class on time."
+print(" He waited", adj3, "but they never came to his aid")
+print("It is a tragic tale but he died as he lived,",verb3,".")
 
