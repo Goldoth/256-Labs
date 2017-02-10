361a4a5463576aad6a78561113258399b91abb2b
Will Debus <wmdebus@syr.edu>
2017-02-09 22:33:04 -0500

    Lesson 3

--- content/lessons/03/Now-You-Code/NYC3-Distance-Conversions.py
+++ content/lessons/03/Now-You-Code/NYC3-Distance-Conversions.py
@@ -18,4 +18,14 @@ you'll need to solve the problem!
 '''
 
 # TODO: Write Todo list then beneath write your code
+#5280 feet in a mile
+#3 feet to a yard
+# input to enter a distance
+# one line to convert feet to yards
+# one line to convert feet to miles
+# and a good old fashioned print line to round it all out
 
+distance = float(input("Please enter a distance in feet: ")
+yards = float(distance / 3)
+miles = float(distance / 5280)
+print("The distance you entered is equal to %.1f yards or %.1f miles") % (yards, miles)                
